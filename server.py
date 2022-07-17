a39d7dfc19f9d231b52b47f65fbce1ea2709687a
Titi923 <petrisor.buciuta92@gmail.com>
2022-07-17 19:50:53 +0300

    First commitgit add .!

---
+++ server.py
@@ -0,0 +1,43 @@
+from lib2to3.pgen2.token import NEWLINE
+from flask import Flask, render_template, request, redirect
+import csv
+
+app = Flask(__name__)
+print(__name__)
+
+@app.route("/")
+def main_page():
+    return render_template("./index.html")
+
+@app.route("/<string:page_name>")
+def about_me(page_name):
+    return render_template(page_name)
+
+def write_to_file(data):
+    with open('./database.txt', mode='a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        file = database.write(f"The email: {email}\nThe Subject: {subject}\nThe message: {message}\n\n")
+
+def write_to_csv(data):
+    with open('./database.csv', mode='a') as database2:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_file(data)
+            write_to_csv(data)
+            return redirect("/thankyou.html")
+        except:
+            return "did not save to database"
+    else:
+        return "Something wrong happened, error!"
